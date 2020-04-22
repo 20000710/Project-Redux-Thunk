8267ccd07f5f7c82ad45c480830569d9aef55db7
zaki <zakielbavaian@gmail.com>
2020-04-22 09:01:22 +0700

    init Todos

---
+++ src/pages/Todos.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from 'react';
+import { getDataAPI } from '../redux/Actions/ActionGetData';   
+  
+import { connect } from 'react-redux';
+
+const Todos = (props) => {
+    console.log("todos", props)
+
+    const [dataName, setDataName] = useState("")
+    const getData = props.getDataAPI;
+
+    useEffect(()=>{
+        getData()       
+    }, [getData])
+
+    const handleAdd = (event) =>{
+        event.preventDefault();
+
+    }
+
+    const handleChange = (event) => {
+        setDataName(event.target.value)
+    }
+    
+    return (
+        <div>
+            <h1>Data Todos</h1>
+            <form onSubmit={handleAdd}>
+                <label>input name todos:</label>
+                <input type="text" name="dataName" value={dataName} onChange={handleChange}/>
+                <input value="add" type="submit"/>
+            </form>
+                
+                {props.dataApi.map((item, index) =>( 
+                    <li style={{marginTop: "15px"}} key={index}>{item.title}</li>                        
+                ))}
+     
+        </div>
+    )
+}
+
+const mapStateToProps = (props) =>{
+    return{
+        dataApi: props.data
+    }
+}
+
+const mapDispatchProps = (dispatch) =>{
+    return{
+        getDataAPI: () => dispatch(getDataAPI())
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchProps)(Todos)
